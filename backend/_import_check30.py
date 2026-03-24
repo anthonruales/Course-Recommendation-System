99a5a34e7338e166652ad7cca171a5beb4df02ed
EnersonNamia <namianamia77@gmail.com>
2026-03-25 01:25:00 +0800

    60 more new questions and options

--- backend/_import_check30.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.insert(0, r'C:\Users\USer\Downloads\capstone-back-end\Course-Recommendation-System\backend')
-from data.questions_enhanced import QUESTIONS_POOL_ENHANCED
-from data.courses_specialized import COURSES_POOL_SPECIALIZED
-print('OK', len(QUESTIONS_POOL_ENHANCED), len(COURSES_POOL_SPECIALIZED))
